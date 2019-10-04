a39c63c0f25dcd581413c95af97572fbc47eb2eb
Vitali Moiseenok <moiseenokv@gmail.com>
2019-10-04 17:30:10 +0300

    feat: add morse decode function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,42 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+var hack = (pair) =>{
+    let out=''
+    switch(pair){
+        case '10':
+            out +='.';
+            break;
+        case '11':
+            out +='-';
+            break;
+        case '*':
+            out +=' '
+            break;
+    }
+    return out;
+}
+
 function decode(expr) {
-    // write your solution here
+    var word = [];
+
+    for (let i=0 , j=0; i<(expr.length/10); i++){
+        word.push(expr.substring(j, j+10));
+        j +=10;
+    }
+
+    return word.map((el)=>{
+        if(el.includes('*')==true){
+            return ' ';
+        }else{
+             let wrd=''
+            for(let i=0, j=0; i<=(el.length/2)-1; i++){
+                wrd += hack(el.substring(j, j+2));
+                j+=2;
+            }
+            return MORSE_TABLE[wrd];
+        }
+    }).join('');
 }
 
 module.exports = {
